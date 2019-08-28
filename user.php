dc4655869752c2f06e8f2293f906424f4137df0d
simonyagas@gmail.com <simonyagas@gmail.com>
2019-08-28 08:47:57 -0300

    primer commit

---
+++ user.php
@@ -0,0 +1,44 @@
+<!-- Consignas:
+    Tenés que crear un sitio web, el cual sea responsive y navegable a través de enlaces 
+    del mismo sitio con las siguientes páginas:
+    1. Home - Con información general del proyecto
+    2. F.A.Q. - Preguntas frecuentes
+    3. Formulario de Registro - Visual, pero no funcional
+    4. Formulario de Login - Visual, pero no funcional
+    5. Formulario de Contacto - Visual, pero no funcional
+    6. Perfil de Usuario
+
+    Para el catálogo de productos (e-commerce)
+
+    Una sección en donde se listen todos los productos de nuestro sistema. 
+    Una sección en donde se pueda ver el detalle de un producto. 
+    Y así mismo una sección en donde podamos ver cómo sería el carrito de compra de nuestra web.
+    • Vista de listado de productos
+    • Vista del detalle de un producto
+    • Vista del carrito de compra
+
+    Formato de entrega:  Ya conocemos GIT, entonces te pedimos que en equipo creen un 
+    repositorio en el cual puedan trabajar y poder revisar modificaciones en equipo, 
+    solucionar problemas si se pisa el código y poder disponer de tu código en cualquier 
+    momento que tengas internet y controlar las versiones del mismo.
+
+     -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Codigo de boostrap -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+     <!-- Codigo de css -->
+     <link rel="stylesheet" href="../css/style.css">
+    <title>Perfil de Usuario</title>
+</head>
+<body>
+    
+</body>
+</html>
